28f7426108fd534190b05e2cf65e1c7c4369efaa
mywns123 <wnsduq2000@naver.com>
2021-06-11 18:02:55 +0900

    준엽 - userInfo(session변경)

--- src/main/java/proj21_funding/controller/UserInfoController.java
+++ src/main/java/proj21_funding/controller/UserInfoController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,10 @@ public class UserInfoController {
 
 	// 회원정보 보기
 	@GetMapping("/account/userInfo")
-	public String form(UserInfo userInfo, HttpSession session) {
+	public String form(UserInfo userInfo, HttpSession session, Model model) {
+		UserAuthInfo userAuthInfo = (UserAuthInfo) session.getAttribute("authInfo");
+		userInfo = service.showUserInfoById(userAuthInfo.getUserId());
+		model.addAttribute(userInfo);
 		return "account/userInfo";
 	}
 
